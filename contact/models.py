344fcebf9ea28b9ebd39cd05c23ab0254e27f0d1
mateusluizz <mateusluizz@hotmail.com>
2023-05-13 13:34:32 -0400

    Django shell

--- contact/models.py
+++ contact/models.py
@@ -12,8 +12,11 @@ from django.utils import timezone
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=15)
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.first_name
