bd062e7e235978c770b89cfea0a6afd85a1e9519
mateusluizz <mateusluizz@hotmail.com>
2023-05-13 14:58:04 -0400

    458. Meta Models

--- contact/models.py
+++ contact/models.py
@@ -4,10 +4,20 @@ from django.utils import timezone
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
+# category (foreign key), show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# owner (foreign key)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=50, default='')
+
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 
 class Contact(models.Model):
@@ -17,6 +27,13 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
-        return self.first_name
+        return f'{self.first_name}'
--- project/settings.py
+++ project/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static/'
 ]
+STATIC_ROOT = BASE_DIR / 'static'  # colletctstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
 # Or
 # STATICFILES_DIRS = (
 #     BASE_DIR / 'base_static',
