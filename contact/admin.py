d6a83ffd2bb9b053c90b6c308891c6cee808431a
mateusluizz <mateusluizz@hotmail.com>
2023-08-19 19:05:42 -0400

    461 - Local Settings

--- contact/admin.py
+++ contact/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 
 from contact import models
 
+# https://docs.djangoproject.com/en/4.2/ref/contrib/admin/
+
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone', 'show',)
     ordering = ('-id',)
-    # list_filter = ('created_date',)
-    search_fields = ('id', 'first_name', 'last_name',)
+    list_filter = ('created_date',)
+    search_fields = ('id', 'first_name', 'last_name', 'phone')
     list_per_page = 20
     list_max_show_all = 100
     list_editable = ('first_name', 'last_name', 'show',)
