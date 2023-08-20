3749149abf2f6fd8187519b2b5ee9666e47cf560
mateusluizz <mateusluizz@hotmail.com>
2023-08-20 19:39:46 -0400

    467 - Template para contato único

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from contact.views import index
+from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('', views.index, name='index'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,21 +1,47 @@
-from django.shortcuts import render
+from django.http import Http404, HttpRequest
+from django.shortcuts import get_object_or_404, render
 
 from contact import models
 
 
-def index(request):
+def index(request: HttpRequest):
     contacts = models.Contact.objects \
         .filter(show=True) \
-        .order_by('-id')[:25]
+        .order_by('-id')[:50]
 
     # print(contacts.query)  # consulta SQL
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
     }
 
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request: HttpRequest, contact_id: int):
+    # single_contact = models.Contact.objects.filter(pk=contact_id).first()
+    # single_contact = get_object_or_404(
+    #     models.Contact.objects.filter(pk=contact_id)
+    # )
+    single_contact = get_object_or_404(
+        models.Contact,
+        pk=contact_id,
+        show=True
+    )
+
+    if single_contact is None:
+        raise Http404('Contact not found!')
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
