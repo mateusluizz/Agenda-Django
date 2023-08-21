b6375c81a32b3804840478cc22832849920a79aa
mateusluizz <mateusluizz@hotmail.com>
2023-08-20 22:03:54 -0400

    477 - request.mehtod e request.POST

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,6 +6,15 @@ from contact import models
 
 def create(request: HttpRequest):
 
+    search = request.POST
+    if request.method == "POST":
+        print(request.method)
+        print(search.get('first_name'))
+        print(search.get('last_name'))
+        print(search.get('password'))
+
+    print(search)
+
     context = {}
 
     return render(
